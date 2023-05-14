7f18711c0e83cba78b9175082e13d14f9940f097
k3nny-wx <k3nny.wx@mailfence.com>
2023-05-14 16:29:06 +0000

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function clearDOM() {
+	document.body.innerHTML = "";
+	const errElem = document.createElement('p');
+	const errText = document.createTextNode("Dev-Tools are deactivated on this site.");
+	errElem.appendChild(errText);
+	document.body.appendChild(errText);
+}
+
+
+console.log(Object.defineProperties(new Error, {
+	message: {get() {clearDOM()}}
+}));
